4bca5b0f0e4e9af4677eade7a0382ad8e9300ac0
fabri <fguespe@gmail.com>
2023-10-06 21:12:55 -0300

    gm

--- src/index.js
+++ src/index.js
@@ -4,9 +4,6 @@ import ReactDOM from "react-dom/client";
 import InboxPage from "./InboxPage-hooks";
 import "./index.css";
 
-// Register service worker
-serviceWorkerRegistration.register();
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
